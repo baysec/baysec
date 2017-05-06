6cfd7bd1c2c0a7872d395d788ceb411c5acc2f08
Nate Lawson <nate@root.org>
2017-05-06 14:45:09 -0700

    Remove duplicate date, add more description

--- gennext.sh
+++ gennext.sh
@@ -31,7 +31,7 @@ esac
 
 cat <<EOF > "$dir/$(date -j -r $gensecs +"%Y-%m-%d")-$(date -j -r $esecs +"%B").html"
 ---
-title: $(date -j -r $esecs +"%B %Y") Baysec
+title: "Next BaySec: $(date -j -r $esecs +'%B %e'$ending', %Y')"
 layout: patriothouse
 event_summary: Baysec
 event_timezone: $TZ
@@ -41,11 +41,8 @@ event_location: Patriot House Pub
 created: !!timestamp '$(date -j -r $gensecs +"%Y-%m-%d %H:%M:%S")'
 ---
 
-# $(date -j -r $esecs +"%B %e$ending, %Y")
-
-Baysec will be at the Patriot House Pub and starts at 7pm.
-
-# Map to Patriot House Pub
+Baysec will be at the Patriot House Pub in SF, starting at 7 pm and usually
+ending some time after 10 pm.
 
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.7895438057476!2d-122.40109834914308!3d37.794971618670836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858061009fbe67%3A0xaca3e8d9f06b5f8e!2sPatriot+House+Pub!5e0!3m2!1sen!2sus!4v1483420387349" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 EOF
